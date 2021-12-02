6a704d73354de860e5695a21865bed1f80998c78
Mamoor khan <khanmamoor007@gmail.com>
2021-12-02 22:20:04 +0530

    changes

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -10,10 +10,12 @@ function Navbar () {
     return (
         <nav className={`${classes.nav} ${classes.container} ${classes.navLinks}`}>
             <img src={Logo} className={classes.logoImg} alt=''/>
+           
             <a className={`${classes.link} ${classes.active}`}><b>Home</b></a>
             <a className={classes.link}><b>Features</b></a>
             <a className={classes.link}><b>Pricing</b></a>
             <a className={classes.link}><b>Blog</b></a>
+           
             <GetStartedBtn />
             
         </nav>
