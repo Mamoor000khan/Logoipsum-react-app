ed8e1519cb0ec8f6a23d60e11803235d9a4c6b3b
Mamoor khan <khanmamoor007@gmail.com>
2021-12-12 08:39:30 +0530

    scroll down btn

--- src/components/Header/headerBottom.js
+++ src/components/Header/headerBottom.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function HeaderBottom() {
+export default function HeaderBottom(props) {
   const classes = useStyles();
 
   function FormRow() {
--- src/components/Header/headerTop.js
+++ src/components/Header/headerTop.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -6,7 +6,26 @@ import GetHeaderBtn from './getHeaderBtn.js';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import Img from '../../assets/img/header.png';
 
+
 export default function HeaderTop() {
+  
+  const [showScroll, setShowScroll] = useState(false)
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 200){
+      setShowScroll(true)
+    } else if (showScroll && window.pageYOffset <= 200){
+      setShowScroll(false)
+    }
+  };
+
+  const scrollEnd = () =>{
+    window.scrollTo({top: document.body.offsetHeight, behavior: 'smooth'});
+  };
+
+  window.addEventListener('scroll', checkScrollTop)
+
+
   return (
     <React.Fragment>
       <Container style={{width:'1151px', height:'636px', marginLeft:'65px'}}>
@@ -19,13 +38,15 @@ export default function HeaderTop() {
                  <p style={{fontSize:'20px', marginTop:'30px', color:'#696871'}}>Outcome-centered product that reduce churm optimize pricing and grow your subscription business end-to-end.</p>
              </Typography>
 
-          <GetHeaderBtn  />
-                 
+          <GetHeaderBtn/>
+                
               <div style={{marginLeft:'257px', marginTop:'-49px'}}>
-                 <ArrowDownwardIcon style={{fontSize:'40px'}}/>
+                <ArrowDownwardIcon onClick={scrollEnd} style={{fontSize:'40px', 
+                                    display: showScroll ? 'flex' : 'none'}}/> 
               </div>
-            
-            <div style={{width:'385', height:'636px', marginLeft: '517px', marginTop: '-504px'}}>
+              
+            <div style={{width:'385', height:'636px', marginLeft: '517px', 
+                         marginTop: '-504px'}}>
                 <img src={Img} alt="" style={{width:'610px'}}/>
             </div>  
       </Container>
