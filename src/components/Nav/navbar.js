34ea02758d3c78c02a863996557f79dd52afbe29
Mamoor khan <khanmamoor007@gmail.com>
2021-12-13 16:32:48 +0530

    fix navbar

--- src/components/Nav/navbar.js
+++ src/components/Nav/navbar.js
@@ -23,18 +23,18 @@ const useStyles = makeStyles((theme) => ({
         textTransform: 'none',
         color: '#696871',
         margin: 'auto',
-        marginLeft:'-54px',
-        marginTop:'19px',
+        // marginLeft:'-54px',
+        marginTop:'17px',
         
       
-        //   '&:hover': {
-        //     borderBottom: '2px solid #5454D4',
-        //     color: 'black',
-        //     lineHeight: 'inherit',
-        //     textDecoration: 'none',
-        //     fontWeight: 'bold',
+          '&:hover': {
+            borderBottom: '2px solid #5454D4',
+            color: 'black',
+            lineHeight: 'inherit',
+            textDecoration: 'none',
+            fontWeight: 'bold',
             
-        // }
+        }
        }
 }));
 
@@ -44,31 +44,32 @@ export default function CenteredGrid() {
   return (
     <Container className={classes.root}>
       <Grid container spacing={3} >
-        <Grid item xs={12} md={3}>
-          <img alt="" src={Logo} style={{width:'222px', height:'41px', margin:'auto',
-                                         textAlign:'center', marginTop:'10px' }}/>
-        </Grid>
-        
-        <Grid  item xs={12} md={1} style={{width:'42px', height:'20px', fontSize:'17px', marginLeft:'52px', margin:'auto', 
-                                            color:'black', /*borderBottom:'2px solid #5454D4'*/ 
-                                             marginTop:'19px'}} >
-                                                 <b> Home</b>
-        </Grid>
-      <Grid className={classes.link} item xs={12} md={1} style={{width:'61px', height:'20px'}}>
-          Features
-        </Grid>
-        <Grid className={classes.link} item xs={12} md={1} style={{width:'47px', height:'20px'}}>
-           Pricing
-        </Grid>
-        <Grid className={classes.link} item xs={12} md={1} style={{width:'30px', height:'20px'}} >
-          Blog
-        </Grid>
+          <Grid item xs={12} md={3}>
+              <img alt="" src={Logo} 
+                 style={{width:'222px', height:'41px', margin:'auto',textAlign:'center', 
+                         marginTop:'10px' }}/>
+          </Grid>
        
-        <Grid item xs={12} md={3}>
-          <Button/>
-        </Grid>
-        </Grid>
-      
+          <Grid  item xs={12} md={1} style={{marginTop:'16px', marginLeft:'56px'}} >
+              <b style={{marginTop:'17px', borderBottom:'2px solid #5454D4'}}> Home</b>
+          </Grid>
+         
+          <Grid  item xs={12} md={1} style={{marginRight:'16px'}} >
+               <p className={classes.link}> Features</p>
+          </Grid>
+        
+          <Grid item xs={12} md={1} style={{marginRight:'17px'}}>
+               <p className={classes.link}> Pricing</p>
+          </Grid>
+          
+          <Grid  item xs={12} md={1} style={{marginRight:'90px'}}>
+               <p className={classes.link}>Blog</p>
+          </Grid>
+       
+          <Grid item xs={12} md={3}>
+               <Button/>
+          </Grid>
+       </Grid>
     </Container>
   );
 }
@@ -81,87 +82,3 @@ export default function CenteredGrid() {
 
 
 
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Button from './getStartedButton';
-
-// import Logo from '../../assets/img/navbarlogo.png';
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     // flexGrow: 1,
-//     padding: '10px',
-//   },
-  
-//   title: {
-//     flexGrow: 1,
-//   },
-
-// link: {
-//     // flexGrow: 1,
-//     fontSize: '1rem',
-//     // fontWeight: '400px',
-//     lineHeight: '41px',
-//     marginLeft: '40px',
-//     textTransform: 'none',
-//     color: '#696871',
-//     // margin: 'auto',
-  
-//       '&:hover': {
-//         borderBottom: '2px solid #5454D4',
-//         color: 'black',
-//         lineHeight: '41px',
-//         textDecoration: 'none',
-//         fontWeight: '50%',
-//     }
-//    }
-//   }
-//  )
-// );
-
-// export default function ButtonAppBar() {
-//   const classes = useStyles();
-
-//   return (
-//     <div className={classes.root}>
-//        <nav style={{marginBottom:'135px'}}>   
-//          <Toolbar style={{width:'1120px', height:'60px', padding:'12px'}}>
-            
-//               <img alt="" src={Logo} style={{width:'200px', height:'41px', marginLeft:'34px'}}/>
-            
-          
-//             <Typography style={{width:'42px' , color:'black', borderBottom:'2px solid #5454D4', lineHeight:'41px', marginLeft: '220px', }} >
-//               Home
-//             </Typography >
-//             <Typography className={classes.link}>
-//               Features
-//             </Typography>
-//             <Typography className={classes.link} >
-//               Pricing
-//             </Typography>
-//             <Typography className={classes.link}>
-//               Blog
-//             </Typography>
-          
-//               <Button/>
-//          </Toolbar>
-//        </nav>
-//     </div>
-//   );
-// }
-    
-
-  // <nav className={`${classes.nav} ${classes.container} ${classes.navLinks}`}>
-        //     <img src={Logo} className={classes.logoImg} alt=''/>
-           
-        //     <a className={`${classes.link} ${classes.active}`}><b>Home</b></a>
-        //     <a className={classes.link}><b>Features</b></a>
-        //     <a className={classes.link}><b>Pricing</b></a>
-        //     <a className={classes.link}><b>Blog</b></a>
-           
-        //     <GetStartedBtn />
-            
-        // </nav>
--- src/components/pricing/pricingpack.js
+++ src/components/pricing/pricingpack.js
@@ -63,12 +63,12 @@ export default function SpacingGrid() {
                              
             
               <Grid className={classes.packItem} >
-                  <Grid ><Paper><p style={{marginTop:'44px'}}>starter</p></Paper></Grid>
-                  <Grid><Paper><b style={{fontSize:'38px'}}><p>Free</p></b></Paper></Grid>
-                   <Grid>   <Paper>   <p>1 Website</p></Paper></Grid>
-                   <Grid><Paper>  <p>5 Gb Hosting</p></Paper></Grid>
-                   <Grid> <Paper><p>Limited Support</p></Paper></Grid>
-                   <Grid> <Paper> <StartedBtn/></Paper></Grid>
+                  <Grid ><p style={{marginTop:'44px'}}>starter</p></Grid>
+                  <Grid><b style={{fontSize:'38px'}}><p>Free</p></b></Grid>
+                   <Grid><p>1 Website</p></Grid>
+                   <Grid><p>5 Gb Hosting</p></Grid>
+                   <Grid><p>Limited Support</p></Grid>
+                   <Grid><StartedBtn/></Grid>
               </Grid>
              
 
@@ -84,34 +84,7 @@ export default function SpacingGrid() {
               </Paper>
             </Grid>
 
-
-
-
-            {/* <Grid  item style={{textAlign:'centre'}}>
-             <Typography className={classes.packItem}>
-             <p>Premium</p>
-           <p>
-             <b style={{fontSize:'45px'}}>$29/month</b></p>
-           <p>10 Website</p>
-           <p>15 Gb Hosting</p>
-           <p>premium Support</p>
-               <StartedBtn/>
-             </Typography>
             </Grid>
-
-            <Grid  item>
-            <Typography className={classes.packItem}>
-            <p>enterprice</p>
-            <p>
-              <b style={{fontSize:'45px'}}>$49/month</b></p>
-            <p>unlimited Website</p>
-            <p>50 Gb Hosting</p>
-            <p>premium Support</p>
-             <StartedBtn/>
-            </Typography>
-            </Grid> */}
-          
-        </Grid>
       </Grid>
       
     </Grid>
@@ -119,108 +92,4 @@ export default function SpacingGrid() {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-
-// import StartedBtn from './startedBtn';
-
-// const useStyles = makeStyles((theme) => ({
-//   packContainer:{
-//         display:'flex',
-//         justifyContent:'spaceBetween',
-//         width:'1202px',
-//         height:'550px'
-//         // textAlign:'centre',
-//     },
-//   packItem: {
-//         width:'330px',
-//          textAlign:'centre',
-//         height:'441px',
-//         // borderRadius:'10px',
-//         marginLeft:'90px',
-//         lineHight:'45px',
-//         color:'#696871',
-//         color:'#1D293F',
-        
-//     '&:hover':{
-//             backgroundColor:'#FF7143',
-//             color:'#fff',
-//             height:'380px',
-//             borderRadius: '15px 15px 15px 15px',
-//         },
-//      },
-//     }));
-
-// function PricingPack() {
-//   const classes = useStyles();
-//     return(
-//       <div className={classes.packContainer}>
-//         <div className={classes.packItem}>
-          
-//           {/* STARTER PACK */}
-//           <p>starter</p>
-//           <p>
-//             <b style={{fontSize:'55px'}}>Free</b></p>
-//           <p>1 Website</p>
-//           <p>5 Gb Hosting</p>
-//           <p>Limited Support</p>
-//             <StartedBtn/>
-//          </div>
-        
-//         {/* PREMIUM PACK */}
-//         <div className={classes.packItem}>
-//           <p>Premium</p>
-//           <p>
-//             <b style={{fontSize:'45px'}}>$29/month</b></p>
-//           <p>10 Website</p>
-//           <p>15 Gb Hosting</p>
-//           <p>premium Support</p>
-//               <StartedBtn/>
-//         </div>
-           
-//            {/* ENTERPRICE */}
-//         <div className={classes.packItem}>
-//            <p>enterprice</p>
-//            <p>
-//              <b style={{fontSize:'45px'}}>$49/month</b></p>
-//            <p>unlimited Website</p>
-//            <p>50 Gb Hosting</p>
-//            <p>premium Support</p>
-//             <StartedBtn/>
-//             </div>
-             
-//        </div>
-//     )
-//   };
-
-//   export default PricingPack;
-             
+            
